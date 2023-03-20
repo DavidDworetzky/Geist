7cff080e0c4f3b711c2e52658911fdae14d66a06
David Dworetzky <djdworetz@gmail.com>
2023-03-19 23:24:16 -0500

    Finish gpt4-completions. (shell service for base agent.) (#2)
    * Finish gpt4-completions.

--- agents/gpt4_agent.py
+++ agents/gpt4_agent.py
@@ -1,10 +1,10 @@
 import requests
-from aents.base_agent import BaseAgent
+from agents.base_agent import BaseAgent
 
 class GPT4Agent(BaseAgent):
     def __init__(self, api_key):
         self.api_key = api_key
-        self.base_url = "https://api.openai.com/v1/engines/gpt-4/completions"
+        self.base_url = "https://api.openai.com/v1/chat/completions"
         self.headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {api_key}"
@@ -12,25 +12,18 @@ class GPT4Agent(BaseAgent):
 
     def complete_text(self, prompt, max_tokens=16, n=1, stop=None, temperature=1.0, top_p=1, frequency_penalty=0, presence_penalty=0, echo=False, best_of=None, prompt_tokens=None, response_format="text"):
         payload = {
-            "prompt": prompt,
+            "messages": [{"role": "user", "content": prompt}],
+            "model": "gpt-4",
             "max_tokens": max_tokens,
             "n": n,
             "temperature": temperature,
             "top_p": top_p,
             "frequency_penalty": frequency_penalty,
             "presence_penalty": presence_penalty,
-            "echo": echo,
-            "response_format": response_format
         }
 
         if stop is not None:
             payload["stop"] = stop
-
-        if best_of is not None:
-            payload["best_of"] = best_of
-
-        if prompt_tokens is not None:
-            payload["prompt_tokens"] = prompt_tokens
 
         response = requests.post(self.base_url, json=payload, headers=self.headers)
 
