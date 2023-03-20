b43e46154045e94e8642ba951a88f6e33f7aa9b8
DavidDworetzky <djdworetz@gmail.com>
2023-03-19 22:45:05 -0500

    Base commit, platz gpt4 agent completion with test endpoint.

---
+++ agents/base_agent.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+
+class BaseAgent(ABC):
+    @abstractmethod
+    def complete_text(self, prompt, max_tokens=16, n=1, stop=None, temperature=1.0, top_p=1, frequency_penalty=0, presence_penalty=0, echo=False, best_of=None, prompt_tokens=None, response_format="text"):
+        pass
---
+++ main.py
@@ -0,0 +1,52 @@
+#framework
+import sys
+from typing import Optional
+import logging
+from app.models.completion import CompleteTextParams
+from agents.gpt4_agent import GPT4Agent
+from fastapi import FastAPI, Depends, HTTPException
+
+#constants
+api_version = 0.1
+enhanced_logging = False
+
+if enhanced_logging:
+    logging.basicConfig()
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+
+#initialization
+app = FastAPI()
+
+	
+#routes
+@app.get('/')
+def version():
+    return {"Version" : f"{api_version}"}
+
+def get_gpt4_client():
+    api_key = "your_openai_api_key_here"
+    return GPT4Agent(api_key)
+
+#basic text completion endpoint for our agent.
+@app.post("/complete_text")
+async def complete_text_endpoint(params: CompleteTextParams, agent: GPT4Agent = Depends(get_gpt4_client)):
+    completions = agent.complete_text(
+        prompt=params.prompt,
+        max_tokens=params.max_tokens,
+        n=params.n,
+        stop=params.stop,
+        temperature=params.temperature,
+        top_p=params.top_p,
+        frequency_penalty=params.frequency_penalty,
+        presence_penalty=params.presence_penalty,
+        echo=params.echo,
+        best_of=params.best_of,
+        prompt_tokens=params.prompt_tokens,
+        response_format=params.response_format
+    )
+
+    if completions:
+        return completions
+    else:
+        raise HTTPException(status_code=500, detail="Failed to generate completions.")
