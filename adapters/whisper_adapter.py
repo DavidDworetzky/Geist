5160f65a2170db4ed6b4961bf217a6f2953ee781
DavidDworetzky <djdworetz@gmail.com>
2023-06-09 01:06:48 -0500

    mms and whisper wip.

--- adapters/whisper_adapter.py
+++ adapters/whisper_adapter.py
@@ -1,16 +1,17 @@
 from abc import ABC, abstractmethod
 from adapters.base_adapter import BaseAdapter
+from typing import List
 
 class WhisperAdapter(BaseAdapter):
     '''
     Whisper Adapter is an adapter for Open AI's speech to text model. 
     '''
-    
+
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def enumerate_actions(self):
-        pass
+    def enumerate_actions(self) -> List[str]:
+        return ["transcribe"]
     
     def transcribe(self, audio_data):
         pass
