09285e8421bda91550a42a3848df4d5f28dae5ac
DavidDworetzky <djdworetz@gmail.com>
2023-08-21 16:49:24 -0500

    Add whisper adapter and completion model to geist.

--- adapters/whisper_adapter.py
+++ adapters/whisper_adapter.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 from adapters.base_adapter import BaseAdapter
 from typing import List
+import requests
+import json
+import openai
+import tempfile
+import os
 
 class WhisperAdapter(BaseAdapter):
     '''
@@ -14,4 +19,19 @@ class WhisperAdapter(BaseAdapter):
         return ["transcribe"]
     
     def transcribe(self, audio_data):
-        pass
+        # Create a temporary file
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+
+        # Write the audio data to the temporary file
+        temp_file.write(audio_data)
+        temp_file.close()
+
+        # Update the audio_file variable to the path of the temporary file
+        audio_file = temp_file.name
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        
+        return transcript
+
+
+
+
