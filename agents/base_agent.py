3aa4e4101944931c117fb2e51514e082b41fed04
David Dworetzky <djdworetz@gmail.com>
2024-04-12 17:03:19 -0400

    Save state persistence. (#28)
    * Save state persistence.

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -4,7 +4,7 @@ from agents.agent_context import AgentContext
 class BaseAgent(ABC):
 
     def __init__(self, agent_context: AgentContext):
-        self.agent_context = agent_context
+        self._agent_context = agent_context
 
     @abstractmethod
     def complete_text(self, prompt, max_tokens=16, n=1, stop=None, temperature=1.0, top_p=1, frequency_penalty=0, presence_penalty=0, echo=False, best_of=None, prompt_tokens=None, response_format="text"):
@@ -15,11 +15,16 @@ class BaseAgent(ABC):
         pass
 
     @abstractmethod
-    def freeze(self):
+    def phase_out(self):
         '''
-        Freeze the agent's state and save it to context.
+        Phase out an agent's state and stop it (aka, stop subprocess, save state)
         '''
         pass
+
+    def phase_in(self):
+        '''
+        Restart an agent's subprocess and rehydrate it (phase in, heh)
+        '''
 
     @abstractmethod
     def tick(self):
--- app/models/database/agent.py
+++ app/models/database/agent.py
@@ -3,6 +3,8 @@ from sqlalchemy import Column, Integer, String, ForeignKey, LargeBinary, DateTim
 from sqlalchemy.orm import relationship, Session
 from app.models.database.database import Base, Session
 from sqlalchemy.dialects.postgresql import insert
+import uuid
+
 
 class Agent(Base):
     '''
@@ -10,8 +12,30 @@ class Agent(Base):
     '''
     __tablename__ = "agent"
     agent_id = Column(Integer, primary_key = True)
+    agent_identifier = Column(String, default=lambda: str(uuid.uuid4()))
     process_id = Column(String)
     world_context = Column(String)
     task_context = Column(String)
     execution_context = Column(String)
+
+
+    @classmethod
+    def get_agent_by_id(cls, agent_id):
+        """
+        Queries the database for an agent with the given agent_id.
+        
+        :param agent_id: The ID of the agent to retrieve.
+        :return: The queried agent object if found, otherwise None.
+        """
+        session = Session()
+        try:
+            agent = session.query(cls).filter_by(agent_id=agent_id).first()
+            return agent
+        except Exception as e:
+            session.rollback()
+            print(f"Failed to query agent by id {agent_id}: {e}")
+            return None
+        finally:
+            session.close()
+
     
