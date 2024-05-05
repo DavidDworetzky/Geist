0bc7ea431dea55c63f8c3a8516722afe839a2bde
David Dworetzky <djdworetz@gmail.com>
2024-05-04 23:27:38 -0500

    change world prompt.

--- agents/gpt4_agent.py
+++ agents/gpt4_agent.py
@@ -8,7 +8,7 @@ import psutil
 
 WORLD_TICK_PROMPT = f"""You are a deep and thorough thinker. 
 Given what you know about the world today, and the main task that you need to complete, consider if there are any additional important facts that you should add to the list of your knowledge. 
-Do not add anything that doesn't need to be added, consolidate anything that is worth consolidating with simpler truths."""
+Do not add anything that doesn't need to be added, consolidate anything that is worth consolidating with simpler facts."""
 
 TASK_TICK_PROMPT = f"You are a driven and focused individual. Given the main task that you wish to complete, and current working subtasks, create a specific list of actionable tasks that will complete your problem. Delimit these as plain english separated by the | character"
 
