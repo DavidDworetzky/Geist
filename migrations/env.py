bd7546fe0ca0e211f9e0da5451ffa9cd3cf630f4
David Dworetzky <djdworetz@gmail.com>
2024-07-20 16:07:13 -0500

    Changes to env.py for initial migration.

--- migrations/env.py
+++ migrations/env.py
@@ -4,6 +4,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from myapp.models import Base
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
