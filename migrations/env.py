4a5adca98a493638d013267d0de1baa4325af997
David Dworetzky <djdworetz@gmail.com>
2024-07-20 17:42:54 -0500

    add proper imports to migration in env.py

--- migrations/env.py
+++ migrations/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from myapp.models import Base
+from app.models.database.database import Base
 
 
 # this is the Alembic Config object, which provides
