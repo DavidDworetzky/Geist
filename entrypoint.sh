143480294d5c82e7aa3fabb85f702001ad71226a
David Dworetzky <djdworetz@gmail.com>
2024-08-25 11:26:20 -0500

    revert entrypoint for docker environment.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,15 @@ set -e
 
 cd /opt/geist
 
+#clean up all existing environments
 conda clean --all --yes
 conda env remove --name geist-linux-docker
-# conda env create -f linux_environment.yml
-conda create --name geist-linux-docker python=3.10
-#conda init
-#conda activate geist-linux-docker
+conda env create -f linux_environment.yml
+
+#init and activate the environment
+conda init
+conda activate geist-linux-docker
 mkdir -p output
 
-#python bootstrap.py
-tail -f /dev/null
+#start our geist server
+python bootstrap.py
