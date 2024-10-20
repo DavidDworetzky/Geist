48f7594b4888ca51d7ddeb18283c4a0a8fbf213c
David Dworetzky <djdworetz@gmail.com>
2024-10-20 12:04:31 -0500

    test script changes.

--- scripts/test_arch_availability.py
+++ scripts/test_arch_availability.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     print(torch.__version__)
     print(torch.cuda.is_available())
     print(torch.backends.mps.is_available())
-    print(torch.backend.mps.is_built())
+    print(torch.backends.mps.is_built())
